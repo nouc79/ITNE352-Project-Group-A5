63a5644f91c6ef36bfde1638bec8a0aeb1dea86f
nouc79 <n.aljabor1421@gmail.com>
2024-12-03 09:36:17 +0300

    project

--- Server.py
+++ Server.py
@@ -1,40 +1,39 @@
-import socket #To create the TCP.
-import threading #To handle multiple client at the same time.
-import json#Saving data in JSON file.
+import socket  # To create the TCP.
+import threading  # To handle multiple clients at the same time.
+import json  # Saving data in JSON file.
 from urllib import request, response 
 import Client
 
-def handle_client(client_socket, client_address) :
-    # I will recvie the clinet name here & store it.
-client_socket.send("Enter your name:")
-Client=client_socket.recv(1000).decode('utf-8')
-print(f"+Connection:{client_address} from {Client}")
+def handle_client(client_socket, client_address):
+    # I will receive the client name here & store it.
+    client_socket.send("Enter your name:".encode('utf-8'))
+    Client = client_socket.recv(1000).decode('utf-8')
+    print(f"+Connection: {client_address} from {Client}")
 
-    # Waiting for the request from Clinet.
-request =client_socket.recv(1000).decode('utf-8')
-if not request : return
-print(f"You get request from {Client}:{request}") 
+    # Waiting for the request from the client.
+    client_request = client_socket.recv(1000).decode('utf-8')
+    if not client_request:
+        return
+    print(f"You get request from {Client}: {client_request}") 
 
-#response, Client=request(Client,answer)
-#handle_client.sendall(response.encode('utf-8'))
+    # Sending a dummy response for now
+    response = {"message": "Your request has been received."}
+    client_socket.send(json.dumps(response).encode('utf-8'))
 
-client_socket.send(json.dumps(response).encode('utf-8'))
+# The server will start and wait for clients
+def start_Server(host="0.0.0.0", port=8999):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))  # Bind requires a tuple (host, port)
+    server_socket.listen(3)
+    print("Server is working, waiting for clients...")
 
-# The server will start and waiting for client 
-def start_Server(host="0.0.0.0",port=8999):
-Server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-Server_socket.listen(3)
-Server_socket.bind(host,port)
-print("Server is working, waiting to client.....")
+    # If true, we will accept new clients
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"New connection from {client_address}")
+        # Handle client in a separate thread
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
 
-# if it true we will accept new client
-while True :
- client_socket, client_address = server_socket.accept()
- print(f"New connection from {client_address}")
-
- start_Server()
-
-#finally:
- #       server_socket.close()
-
-#def handle_request(request):
+# Start the server
+start_Server()
